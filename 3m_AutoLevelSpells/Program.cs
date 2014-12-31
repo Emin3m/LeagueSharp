50caff74fd6a1465b994a638b117d45406858f05
Emin3m <x22@polizisten-duzer.de>
2014-12-31 15:41:47 +0100

    1

--- 3m_AutoLevelSpells/Program.cs
+++ 3m_AutoLevelSpells/Program.cs
@@ -101,7 +101,7 @@ namespace AutoLevelSpell
             Game.PrintChat("[00:00] <font color='#C80046'>Note: 1 = Q - 2 = W - 3 = E - 4 = R.</font>");
             Game.PrintChat("[00:00] <font color='#C80046'>Note: Wrong or impossible sequences are not catched!!!</font>");
             Game.OnGameProcessPacket += Game_OnGameProcessPacket;
-            
+            Drawing.DrawText(50, 10, System.Drawing.Color.White, "Sample Text");
         }
 
         private static void Game_OnGameProcessPacket(EventArgs args)
